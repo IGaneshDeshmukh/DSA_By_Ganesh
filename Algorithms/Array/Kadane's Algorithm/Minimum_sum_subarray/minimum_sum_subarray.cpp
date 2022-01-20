ef2016d78cf1eb42f3096a8e5302fe36b45c6915
Ganesh Deshmukh <ganesha.deshmukh@outlook.com>
2022-01-20 17:43:36 +0530

    Kadane's min_sum problem  modified

--- Algorithms/Array/Kadane's Algorithm/Minimum_sum_subarray/minimum_sum_subarray.cpp
+++ Algorithms/Array/Kadane's Algorithm/Minimum_sum_subarray/minimum_sum_subarray.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+using namespace std;
 
 int min_sum_subarray(int array[], int n){
-    int min_sum = INT_MAX;
+    
+	int min_sum = INT_MAX;
     int cur_min = 0;
 
     for(int i = 0; i < n; i++){
         cur_min = cur_min + array[i];
 
-        if( cur_min > min_sum){
+        if( cur_min < min_sum){
             min_sum = cur_min;
         }
         if(cur_min > 0){
             cur_min = 0;
         }
     }
+    return min_sum;
 }
 
 
 int main(){
-    int arr_1[] = {5, 6, 7, -2}
+    int arr_1[] = {5, 6, 7, -2};
     int arr_2[] = {2, -5, 3, -8};
     cout<<"min_sum_of_subarray of {5,6,7,-2} is "<<min_sum_subarray(arr_1,4);
-    cout<<"min_sum_of_subarray of {2,-5,3,-8} is "<<min_sum_subarray(arr_2,4);
+    cout<<"\nmin_sum_of_subarray of {2,-5,3,-8} is "<<min_sum_subarray(arr_2,4);
+    return 0;
 }
